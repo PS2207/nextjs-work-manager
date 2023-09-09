35f9fac5ae8ded43115ef444552013b16cc52786
PS2207 <pragyasinha2207@gmail.com>
2023-09-09 15:53:37 +0530

    Create page.js

---
+++ src/app/profile/user/page.js
@@ -0,0 +1,9 @@
+import ShowUser from "./ShowUser"
+
+export const metadata={
+    title: "User Profile : Work Manager(LoggedInUser)"
+}
+const UserProfile=()=>{
+    return <ShowUser/>
+}
+export default UserProfile
