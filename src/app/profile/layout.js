acdc061c6a6ffdfcec2b3b05dd25471c894e1bfc
PS2207 <pragyasinha2207@gmail.com>
2023-09-09 17:07:10 +0530

    Create layout.js

---
+++ src/app/profile/layout.js
@@ -0,0 +1,10 @@
+export default function Layout({children}){
+    return(
+        <div>
+        {/* <h1>This is profile header</h1> */}
+        {children}
+        {/* <h1>This is profile Footer</h1> */}
+    </div>
+    )
+}
+
